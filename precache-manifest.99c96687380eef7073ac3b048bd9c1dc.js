41ac1e8487451b3c0ae0ead31cbc410c101ffa34
Matsumoto Miu <miu.matsumoto04@gmail.comgit config --global user.email miu.matsumoto04@gmail.com>
2020-05-04 18:28:21 +0900

    Updates

---
+++ precache-manifest.99c96687380eef7073ac3b048bd9c1dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "888bc5a63d7956dcd07fac653116bdd6",
+    "url": "/task-manager-app/index.html"
+  },
+  {
+    "revision": "cf2e45a7aa47d35374ca",
+    "url": "/task-manager-app/static/css/main.50b6df77.chunk.css"
+  },
+  {
+    "revision": "8e5ff881e246d2eb6e6a",
+    "url": "/task-manager-app/static/js/2.771bd6b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/task-manager-app/static/js/2.771bd6b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf2e45a7aa47d35374ca",
+    "url": "/task-manager-app/static/js/main.f4ba39b9.chunk.js"
+  },
+  {
+    "revision": "38c40cc3b9ad6ee09d86",
+    "url": "/task-manager-app/static/js/runtime-main.d50a5442.js"
+  }
+]);
